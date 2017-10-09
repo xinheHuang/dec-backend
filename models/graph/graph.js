32359ec8da061509d2404d7f07462107691634b2
xinhe huang <huangxinhe@huayun.com>
2017-10-09 22:49:29 +0800

    add uid to graph

--- models/graph/graph.js
+++ models/graph/graph.js
@@ -6,7 +6,7 @@ const database = require('./db/index')
 const Graph = database.define('graph', {
     GID: { type: 'INTEGER', primaryKey: true,autoIncrement: true, },
     entity: { type: 'TEXT', allowNull: false },
-    author: { type: 'TEXT', allowNull: false },
+    UID: { type: 'INTEGER', allowNull: false },
     riqi: { type: 'DATETIME', allowNull: false },
     company: { type: 'TEXT', allowNull: false },
     type: { type: 'INTEGER', allowNull: false },
