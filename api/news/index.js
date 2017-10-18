8bf6a91ecc019827384c05074c8db913858bec34
xinhe huang <huntriver@hotmail.com>
2017-10-18 23:04:10 +0800

    'fixed'

--- api/news/index.js
+++ api/news/index.js
@@ -37,8 +37,11 @@ const apis = {
             const find = (ID) => {
                 return categories
                     .filter((category) => category.FCID == ID)
-                    .map(({ CID, name, FCID, tier }) => ({
-                        CID, name, FCID, tier,
+                    .map(({CID, name, FCID, tier}) => ({
+                        CID,
+                        name,
+                        FCID,
+                        tier,
                         subs: find(CID)
                     }))
             }
@@ -51,74 +54,47 @@ const apis = {
         method: 'get',
         url: '/newsCategory/:CID/news',
         async handler(ctx, next) {
-            const { CID } = ctx.params
-            const { pageNumber, pageSize } = ctx.request.query
+            const {CID} = ctx.params
+            const {pageNumber, pageSize} = ctx.request.query
 
             const childCID = ((await NewsCategory.findAll({
-                where: {
-                    FCID: CID
-                }
-            })) || []).map((c) => c.CID)
+                                                              where: {
+                                                                  FCID: CID
+                                                              }
+                                                          })) || []).map((c) => c.CID)
 
             console.log('childCID', childCID)
 
 
-            const res = await News.findAll({
-                include: [
-                    {
-                        model: NewsCategory,
-                        where: {
-                            CID: {
-                                $in: [...childCID, CID]
-                            }
-                        },
-                        required: true,
-                        duplicating: false,
-                    }
-                ],
-                order: [
-                    ['riqi', 'DESC'],
-                ],
-                attributes: { exclude: ['content'] },
-                limit: 10,
-                offset: 0
-            })
-            // const res = await NewsCategory.findAll({
-            //     where: {
-            //         CID: {
-            //             $in: [...childCID, CID]
-            //         }
-            //     },
-            //     include: [
-            //         {
-            //             model: NewsRelation,
-            //             include: [{
-            //                 model: News,
-            //                 attributes: { exclude: ['content'] },
-            //                 }
-            //             ],
-            //             order: [
-            //                 [News,'riqi', 'DESC'],
-            //             ],
-            //             offset: (pageNumber - 1) * pageSize,
-            //             limit: +pageSize,
-            //         }
-            //     ]
-            // })
-            ctx.body = res
-            // const category = await NewsCategory.findById(CID)
-            // const [news, count] = await Promise.all([category.getNews({
-            //     attributes: { exclude: ['content'] },
-            //     offset: (pageNumber - 1) * pageSize,
-            //     limit: +pageSize,
-            //     order: [
-            //         ['riqi', 'DESC'],
-            //     ]
-            // }), category.countNews()])
-            // ctx.body = {
-            //     news,
-            //     totalPage:Math.ceil(count/pageSize)
-            // }
+            const {count,rows} = await News.findAndCount(
+                {
+                    include: [
+                        {
+                            model: NewsCategory,
+                            where: {
+                                CID: {
+                                    $in: [...childCID, CID]
+                                }
+                            },
+                            attributes: {exclude: ['news_relation']},
+                            required: true,
+
+                            // duplicating: false,
+                        }
+                    ],
+                    order: [
+                        ['riqi', 'DESC'],
+                    ],
+                    subQuery: false,
+                    attributes: {exclude: ['content']},
+                    offset: (pageNumber - 1) * pageSize,
+                    limit: +pageSize,
+                })
+            // rows.forEach(())
+            ctx.body = {
+                pageCount:Math.ceil(count/pageSize),
+                news:rows
+            }
         }
     }
 
