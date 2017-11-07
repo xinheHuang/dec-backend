d97a2874c1ec57c6f9cec57eea797b26695ff094
xinhe huang <huangxinhe@huayun.com>
2017-11-07 14:36:05 +0800

    add ifc table

---
+++ models/db/index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Xinhe on 2017-09-20.
+ * 数据库实例初始化
+ */
+const config = require('../../conf/dbConfig')
+const Sequelize = require('sequelize')
+const {  username, password, host, port } = config
+const sequelize = new Sequelize('IFC', username, password, {
+    host,
+    port,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000
+    },
+    define: {
+        timestamps: false,
+        freezeTableName: true,
+    }
+})
+
+module.exports = sequelize
